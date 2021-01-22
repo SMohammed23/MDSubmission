8ade214fce87c4f80ebe3b0bbddd3cee1682744f
SaifuddinMo <saifumsn@gmail.com>
2021-01-22 17:55:26 -0500

    FInal Submission

---
+++ MDCode/InitEllip.m
@@ -0,0 +1,26 @@
+doPlot = 1;
+dt = 15e-15;
+TStop = 1000 * dt;
+InitDist = 0.0;
+Method = 'VE'; % VE -- verlot; FD -- Forward Difference
+
+Mass0 = MassMx * C.am; % Silicon
+Mass1 = MassMn * C.am; % Argon
+
+AtomSpacing = 0.5430710e-9;
+LJSigma = AtomSpacing / 2^(1/6);
+LJEpsilon = 1e-21;
+
+PhiCutoff = 3 * AtomSpacing * 1.1;
+
+T = 30;
+
+AddEllipAtomicArray(20, 0, 0, 0, 0, 0, T, 0);
+
+Size = 20 * AtomSpacing;
+Limits = [-Size +Size -Size +Size]; % square is good
+PlDelt = 5*dt;
+MarkerSize = 10;
+PlotFile = 'Block.gif';
+doPlotImage = 0;
+PlotSize = [100, 100, 1049, 1049];
